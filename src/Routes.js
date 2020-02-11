3248c271a0f5da64baa413fae9f9e89fc8f5f5cc
dione616 <Vetal160199@i.ua>
2020-02-11 18:22:28 +0300

    Created Private RoutesL42

--- src/Routes.js
+++ src/Routes.js
@@ -4,11 +4,18 @@ import { Switch, Route } from "react-router-dom"
 import Home from "./components/home"
 import SignIn from "./components/signin/index"
 import Dashboard from "./components/admin/Dashboard"
+import PrivateRoutes from "./components/authRoutes/privateRoutes"
 
 const Routes = props => {
   return (
     <Layout>
       <Switch>
+        <PrivateRoutes
+          {...props}
+          path="/dashboard"
+          exact
+          component={Dashboard}
+        />
         <Route path="/dashboard" exact component={Dashboard} />
         <Route path="/sign_in" exact component={SignIn} />
         <Route path="/" exact component={Home} />
--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom"
 import "./Resources/css/app.css"
 import { BrowserRouter } from "react-router-dom"
 import Routes from "./Routes"
+import { firebase } from "./firebase"
 
-const App = () => {
+const App = props => {
   return (
     <BrowserRouter>
-      <Routes />
+      <Routes {...props} />
     </BrowserRouter>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById("root"))
+firebase.auth().onAuthStateChanged(user => {
+  ReactDOM.render(<App user={user} />, document.getElementById("root"))
+})
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
